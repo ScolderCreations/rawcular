191ebf18f362ed2748f6e51994eca7e66bf06b23
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-07 17:41:48 -0700

    Update search.js

--- search.js
+++ search.js
@@ -1,2 +1,8 @@
-let parameters = window.prompt("insert search parameters","");
-const results = JSON.parse("");
+async function search(param) {
+  let site = await fetch("https://scratchdb.lefty.one/v3/forum/search?q=", param, "&o=&page=:page");
+  let result = await site.json();
+  return result
+}
+
+let paramara = window.prompt("insert search parameters","");
+const results = JSON.parse(search(paramara));
